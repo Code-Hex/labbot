0d6a01c1ebda1eb345e9f031af669682274ae8f3
K <x00.x7f@gmail.com>
2017-06-13 20:15:39 +0900

    Completed linebeacon

--- line-beacon.go
+++ line-beacon.go
@@ -4,10 +4,27 @@ import (
 	"fmt"
 	"net/http"
 
+	"time"
+
 	"github.com/line/line-bot-sdk-go/linebot"
 	"github.com/nlopes/slack"
 	"go.uber.org/zap"
 )
+
+type timezone int
+
+const (
+	Morning timezone = iota
+	Daytime
+	Night
+	MidNight
+)
+
+var timeStamp map[string]time.Time
+
+func init() {
+	timeStamp = make(map[string]time.Time)
+}
 
 func (l *labbot) fromBeacon(events []*linebot.Event, r *http.Request) {
 	api := slack.New(slackToken)
@@ -17,31 +34,62 @@ func (l *labbot) fromBeacon(events []*linebot.Event, r *http.Request) {
 		return
 	}
 
-	var channelID string
-	for _, channel := range channels {
-		if channel.Name == "tamaki" {
-			channelID = channel.ID
-			break
-		}
+	channelID, err := findChannelID(channels, "timestamp")
+	if err != nil {
+		l.Warn("Failed to find channel id", zap.Error(err))
+		return
 	}
 
 	for _, event := range events {
+		if event.Type == linebot.EventTypeMessage {
+			bot, err := linebot.New(channelSecret, channelToken)
+			if err != nil {
+				l.Error("Failed to construct linebot", zap.Error(err))
+				return
+			}
+			_, err = bot.ReplyMessage(
+				event.ReplyToken,
+				linebot.NewTextMessage("こんにちは！"),
+			).Do()
+			if err != nil {
+				l.Error("Failed to reply message", zap.Error(err))
+				return
+			}
+		}
 		if event.Type == linebot.EventTypeBeacon {
 			src := event.Source
 			userID := src.UserID
 			bot, err := linebot.New(channelSecret, channelToken)
 			if err != nil {
 				l.Error("Failed to construct linebot", zap.Error(err))
+				return
 			}
 			res, err := bot.GetProfile(userID).Do()
 			if err != nil {
 				l.Error("Failed to get user profile", zap.Error(err))
+				return
 			}
 
 			switch event.Beacon.Type {
 			case linebot.BeaconEventTypeEnter:
+				_, err := bot.ReplyMessage(
+					event.ReplyToken,
+					linebot.NewTextMessage(fmt.Sprintf("%sさん%s♡", res.DisplayName, greeting())),
+				).Do()
+				if err != nil {
+					l.Error("Failed to reply message", zap.Error(err))
+					return
+				}
 				l.welcomeToLab(api, res.DisplayName, channelID)
 			case linebot.BeaconEventTypeLeave:
+				_, err := bot.ReplyMessage(
+					event.ReplyToken,
+					linebot.NewTextMessage(fmt.Sprintf("%sさん、%s", res.DisplayName, getMessageWorkingTime(res.DisplayName))),
+				).Do()
+				if err != nil {
+					l.Error("Failed to reply message", zap.Error(err))
+					return
+				}
 				l.seeyouFromLab(api, res.DisplayName, channelID)
 			}
 		}
@@ -49,61 +97,107 @@ func (l *labbot) fromBeacon(events []*linebot.Event, r *http.Request) {
 }
 
 func (l *labbot) welcomeToLab(api *slack.Client, name, channelID string) {
-	params := slack.PostMessageParameters{}
+	now := time.Now()
+	timeStamp[name] = now
+	formatted := now.Format("2006年01月02日 15時04分")
+	msg := fmt.Sprintf("%sさんが%sに来ました♡", name, formatted)
+	params := parameter()
 	attachment := slack.Attachment{
-		Color:   "#e74c3c",
-		Pretext: "some pretext",
-		Text:    "some text",
-		// Uncomment the following part to send a field too
-		/*
-			Fields: []slack.AttachmentField{
-				slack.AttachmentField{
-					Title: "a",
-					Value: "no",
-				},
-			},
-		*/
+		Color: "#e67e22",
+		Text:  msg,
 	}
 	params.Attachments = []slack.Attachment{attachment}
-	msg := fmt.Sprintf("こんにちは!!, %sさん♡", name)
-	_, timestamp, err := api.PostMessage(channelID, msg, params)
+	_, timestamp, err := api.PostMessage(channelID, "", params)
 	if err != nil {
 		l.Warn(`Failed to post "welcome message" to slack`, zap.Error(err))
 		return
 	}
 	l.Info(
-		"Message successfully sent to channel",
+		"Message successfully sent to slack",
 		zap.String("channelID", channelID),
 		zap.String("timestamp", timestamp),
 	)
 }
 
 func (l *labbot) seeyouFromLab(api *slack.Client, name, channelID string) {
-	params := slack.PostMessageParameters{}
+	now := time.Now()
+	formatted := now.Format("2006年01月02日 15時04分")
+	msg := fmt.Sprintf("%sさんが%sに帰りました♡", name, formatted)
+	params := parameter()
 	attachment := slack.Attachment{
-		Color:   "#e74c3c",
-		Pretext: "some pretext",
-		Text:    "some text",
-		// Uncomment the following part to send a field too
-		/*
-			Fields: []slack.AttachmentField{
-				slack.AttachmentField{
-					Title: "a",
-					Value: "no",
-				},
-			},
-		*/
+		Color: "#3498db",
+		Text:  msg,
 	}
 	params.Attachments = []slack.Attachment{attachment}
-	msg := fmt.Sprintf("また来てくださいね!!, %sさん♡", name)
-	_, timestamp, err := api.PostMessage(channelID, msg, params)
+	_, timestamp, err := api.PostMessage(channelID, "", params)
 	if err != nil {
 		l.Warn(`Failed to post "seeyou message" to slack`, zap.Error(err))
 		return
 	}
 	l.Info(
-		"Message successfully sent to channel",
+		"Message successfully sent to slack",
 		zap.String("channelID", channelID),
 		zap.String("timestamp", timestamp),
 	)
+}
+
+func findChannelID(channels []slack.Channel, name string) (string, error) {
+	for _, channel := range channels {
+		if channel.Name == name {
+			return channel.ID, nil
+		}
+	}
+	return "", fmt.Errorf("Could not find ChannelID of #%s", name)
+}
+
+func parameter() slack.PostMessageParameters {
+	return slack.PostMessageParameters{
+		Username: "kirari",
+		AsUser:   true,
+	}
+}
+
+func greeting() string {
+	switch getTimeZone() {
+	case Morning:
+		return "おはようございます"
+	case Daytime:
+		return "こんにちは"
+	case Night:
+		return "こんばんは"
+	}
+	return "夜遅くまでお疲れ様です"
+}
+
+func getTimeZone() timezone {
+	hour := time.Now().Hour()
+
+	if 11 <= hour && hour < 17 {
+		return Daytime
+	}
+	if 17 <= hour && hour < 23 {
+		return Night
+	}
+	if 5 <= hour && hour < 11 {
+		return Morning
+	}
+
+	return MidNight
+}
+
+func getMessageWorkingTime(name string) string {
+	came, ok := timeStamp[name]
+	if !ok {
+		return "またねー！"
+	}
+	sub := int(time.Now().Sub(came).Hours())
+	if 0 <= sub && sub <= 4 {
+		return "お疲れ様です！"
+	}
+
+	if 4 <= sub && sub <= 8 {
+		return "とっても頑張ったね！偉い！"
+	}
+
+	return "お願い！死なないでね！"
 }
