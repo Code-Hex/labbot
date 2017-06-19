8e4218611988b14a55bd9fb271b2e5eeecb6858e
K <x00.x7f@gmail.com>
2017-06-19 11:56:46 +0900

    Modified timing for cron job

--- labbot.go
+++ labbot.go
@@ -160,7 +160,7 @@ func (l *labbot) prepare() error {
 func (l *labbot) registerCronHandlers() {
 	l.Info("register cron")
 	// Please check cron.go
-	l.AddFunc("0 30 18 * * *", l.isThereProgress)
+	l.AddFunc("0 30 18 * * 1-5", l.isThereProgress)
 	l.AddFunc("0 0 10 * * 5", l.noticeSeminar)
 
 	l.Start() // start cron job
